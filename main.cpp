9966f8937da5d807d02d3e9b66876d988fe98d06
YanqingXu <xyq949587362xyq007@163.com>
2021-02-10 21:45:20 +0800

    add windows socket

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
-#include<iostream>
-using namespace std;
+#define WIN32_LEAN_AND_MEAN
+#include<windows.h>
+#include<WinSock2.h>
+//#pragma comment(lib, "ws2_32.lib")	已手动添加到项目属性链接器里的依赖附加项里
 
 int main()
 {
-	cout << "Hello, World!" << endl;
+	WORD ver = MAKEWORD(2, 2);
+	WSADATA data;
+	WSAStartup(ver, &data);
+
+	WSACleanup();
 	return 0;
 }
